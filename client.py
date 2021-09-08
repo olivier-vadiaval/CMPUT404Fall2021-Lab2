9c7411a31a103b2c49529a6fc324727e886da87c
Olivier Vadiavaloo <vadiaval@ualberta.ca>
2021-09-08 00:43:21 -0400

    Rename socket to client_socket

--- client.py
+++ client.py
@@ -7,15 +7,17 @@ BUF_SIZE = 4096
 page_request = "GET / HTTP/1.1\r\nHost: %s\r\n\r\n" % HOST
 
 try:
-    with socket.create_connection(address) as sc:
+    with socket.create_connection(address) as client_socket:
+        # Using create_connection is equivalent to using the
+        # socket method and then using the connect method
         print('Connection established')
         
-        sc.sendall(page_request.encode())
+        client_socket.sendall(page_request.encode())
         print('Request sent!')
 
         data = ""
         while True:
-            received_data = sc.recv(BUF_SIZE)
+            received_data = client_socket.recv(BUF_SIZE)
             if not received_data:
                 break
             data += received_data.decode()
