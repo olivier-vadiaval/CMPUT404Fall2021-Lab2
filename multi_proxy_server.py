49c4f39c4101ec8e5ec702cc8f176bf7732197c2
Olivier Vadiavaloo <vadiaval@ualberta.ca>
2021-09-15 19:54:12 -0400

    Rename proxy_server.py to multi_proxy_server.py

--- multi_proxy_server.py
+++ multi_proxy_server.py
@@ -28,27 +28,27 @@ def conn_handler(conn_socket, client_socket):
         conn_socket.close()
 
 def main():
-    GOOGLE = "www.google.com"   # Google hostname
+    EXTERN_HOST = "www.google.com"   # Google hostname
     HOST = "127.0.0.1"          # localhost
     PORT = 8001                 # port number
     address = (HOST, PORT)
 
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-            client_socket.connect((GOOGLE, 80))
-            print("Connected to Google")
 
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-                # Reuse port number
-                server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            # Reuse port number
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
-                server_socket.bind(address)
-                print("Proxy server binded")
+            server_socket.bind(address)
+            print("Proxy server binded")
 
-                print("Listening...")
-                server_socket.listen()
+            print("Listening...")
+            server_socket.listen()
 
-                while True:
+            while True:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+                    client_socket.connect((GOOGLE, 80))
+                    print("Connected to Google")
                     conn_socket, client_addr = server_socket.accept()
 
                     client_host, client_port = client_addr
