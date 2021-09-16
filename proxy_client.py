0d4e77d742c44bfdd8e8909febecda0b143d9bd3
Olivier Vadiavaloo <vadiaval@ualberta.ca>
2021-09-15 23:15:39 -0400

    Reple disconnection prompt

--- proxy_client.py
+++ proxy_client.py
@@ -5,24 +5,24 @@ BUF_SIZE = 4096
 
 def main():
 
-    GOOGLE = "www.google.com"
-    HOST = "127.0.0.1"  # localhost
+    GOOGLE_HOSTNAME = "www.google.com"
+    HOST = "localhost"  # localhost
     PORT = 8001         # port number
     address = (HOST, PORT)
 
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             client_socket.connect(address)
-            print("Established connection with server!")
+            print("Established connection with proxy server!")
 
             while True:
                 # Send a GET request
-                get_request = f"GET / HTTP/1.1\r\nHost: {GOOGLE}\r\n\r\n"
-                client_socket.sendall(get_request.encode()) 
+                http_request = f"GET / HTTP/1.1\r\nHost: {GOOGLE_HOSTNAME}\r\n\r\n"
+                client_socket.sendall(http_request.encode()) 
                 response_data = client_socket.recv(BUF_SIZE)
 
                 print(response_data)
-                exit_input = input("Exit? (y/n)").lower()
+                exit_input = input("Disconnect? (y/n)").lower()
                 
                 if exit_input == "y":
                     break
